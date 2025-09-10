f693c3a606d5e50269b8a61a5ceaa2f492505779
Mallaraj <mirjan01allaraj@gmial.com>
2025-09-10 14:49:36 +0200

    Testing functions

--- webapp/Header.test.js
+++ webapp/Header.test.js
@@ -3,5 +3,5 @@ import { Header } from './Header.js';
 test('Gjeneron një titull HTML me tekstin e pritshëm', () => {
   const element = Header();
   expect(element.tagName).toBe('H1');
-  expect(element.textContent).toBe('Mirë se erdhe në aplikacionin DevOps');
+  expect(element.textContent).toBe('Mirë se erdhe në testin e aplikacionin DevOps');
 });
